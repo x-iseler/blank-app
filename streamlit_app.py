39558bbc01c663d427498adc59414d57bbec525e
x-iseler <xiseler@iliad-free.fr>
2025-01-20 13:19:05 +0000

    tableau

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new streamlit app")
-st.write(
-    "Let's start ! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Visualisation")
+
+st.dataframe(data= None)
+
