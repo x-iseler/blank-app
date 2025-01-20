f445f26cdaa669b1518ebd81e731cc0e06056e8c
x-iseler <xiseler@iliad-free.fr>
2025-01-20 13:13:25 +0000

    test02

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new streamlit app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start ! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
