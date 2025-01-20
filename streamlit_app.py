2c77dad0ef19cd21e7d355a4e64c7c058de65019
x-iseler <xiseler@iliad-free.fr>
2025-01-20 13:52:19 +0000

    tableau test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'A':[0,1,2,3,4,5,6,7,8,9],
+    'B':[10,11,12,13,14,15,16,17,18,19],
+    'year':['->2022','2023','->2022','2023','2024','2025','2024','2025','2024','2025']
+    })
 
 st.title("Visualisation")
 
-st.dataframe(data= None)
+
+st.dataframe(df)
 
