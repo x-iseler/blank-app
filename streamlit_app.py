d2c4d5a97062d4be89e910303001d9aa1d6de75b
x-iseler <xiseler@iliad-free.fr>
2025-01-21 14:50:51 +0000

    All ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,35 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+listA = [0,1,2,3,4,5,6,7,8,9]
+listB = [10,11,12,13,14,15,16,17,18,19]
+listYear = ['->2022','2023','->2022','2023','2024','2025','2024','2025','2024','2025']
 
 df = pd.DataFrame({
-    'A':[0,1,2,3,4,5,6,7,8,9],
-    'B':[10,11,12,13,14,15,16,17,18,19],
-    'year':['->2022','2023','->2022','2023','2024','2025','2024','2025','2024','2025']
+    'A':listA,
+    'B':listB,
+    'year':listYear
     })
-
-dfAll = pd.DataFrame({
-    'A':['All'],
-    'B':['All'],
-    'year':['All']
-    })
-
 
 st.title("Visualisation")
+
+listA = ['All'] + list(sorted(set(listA)))
+listB = ['All'] + list(sorted(set(listB)))
+listYear = ['All'] + list(sorted(set(listYear)))
 
 st.dataframe(df)
 
 firstExpander = st.sidebar.expander('test 1')
-
 firstExpander.selectbox("Year",
-                        df['year'].unique()
+                        listYear
                         )
 firstExpander.selectbox("A",
-                        df['A'].unique()
+                        listA
                         )
 firstExpander.button("Filter", "2", None, None, None,None)
 firstExpander.selectbox("B",
-                        df['B'].unique()
+                        listB
                         )
 firstExpander.button("Filter", "3", None, None, None,None)
 
